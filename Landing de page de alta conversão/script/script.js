9e13cee289c8c7eace7143145a5395d9f0f87f2f
Pedro de Souza <131737607+Pedro-OSouza@users.noreply.github.com>
2023-06-10 02:40:48 -0300

    Pequenas alterações nos códigos
    Pequenas alterações nos códigos da landing page com o intuito de torna-la mais legível e mais clean

--- Landing de page de alta conversão/script/script.js
+++ Landing de page de alta conversão/script/script.js
@@ -8,15 +8,17 @@ var menu_lateral = document.querySelector(".menu-lateral"),
 btn_menu.addEventListener("click", exibir);
 
 function exibir() {
-            if (menu_lateral.style.marginLeft == "0%" && largura <= 600){
+            if (menu_lateral.style.display == "none"){
+        btn_menu.style.marginLeft = "-45%";
         menu.style.display = "block";
-        menu_lateral.style.marginLeft = "-35%"
+        menu_lateral.style.display = "block"
         blur.style.display = "block"
         blur.style.transition = "0.5s"
         menu_lateral.style.transition = "0.5s"
         } else {
+            btn_menu.style.marginLeft = "-15%"
             menu.style.display = "block";
-        menu_lateral.style.marginLeft = "0%"
+        menu_lateral.style.display = "none"
         blur.style.display = "none"
         blur.style.transition = "0.5s"
         menu_lateral.style.transition = "0.5s"
